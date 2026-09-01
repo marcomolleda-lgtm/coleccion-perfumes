3620b3e66b9451d99276d78b8feddaf856c543a7
marcomolleda-lgtm <marcomolleda@gmail.com>
2026-09-01 10:56:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'perfumes-v6';
+const CACHE = 'perfumes-v7';
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(CACHE).then(function(cache) {
-      return cache.addAll(['./index.html', './manifest.json', './icon-192-v6.png', './icon-512-v6.png', './icon-192-maskable-v6.png', './icon-512-maskable-v6.png']);
+      return cache.addAll(['./index.html', './manifest.json', './icon-192-v7.png', './icon-512-v7.png', './icon-192-maskable-v7.png', './icon-512-maskable-v7.png']);
     })
   );
   self.skipWaiting();
